75d35031774809380552d1c2a3ecfc12141cc2c1
ben256dev <benjamin@ben256.com>
2024-11-30 21:17:02 -0700

    trying to init as test

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_init.h>
+
 #include <stdio.h>
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
+    SDL_Init(SDL_INIT_AUDIO |
+             SDL_INIT_VIDEO |
+             SDL_INIT_JOYSTICK |
+             SDL_INIT_HAPTIC |
+             SDL_INIT_GAMEPAD |
+             SDL_INIT_EVENTS |
+             SDL_INIT_SENSOR |
+             SDL_INIT_CAMERA );
+
     printf("Hello world!\n");
     return 0;
 }
